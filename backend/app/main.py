40a1a4343293ad5a3e5964f90edd83736f85bec2
firasnazar2004 <22703822@emu.edu.tr>
2025-08-21 15:40:31 +0300

    changed CORSMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
